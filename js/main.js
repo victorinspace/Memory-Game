9628442ca46862e53426e427bb045d7896de1eac
Victor Velasquez <victorv24@gmail.com>
2018-07-20 09:48:40 -0700

    basic HTML and CSS

---
+++ js/main.js
@@ -0,0 +1,8 @@
+// Deliverables:
+// 1. Have a "new game" screen with a single choice "new game"
+// 2. The game should have a total of 9 pairs (18 cards total)
+// 3. When the user runs out of turns, show a losing screen
+// 4. When the user finds a match, leave the cards face up and disallow clicking those cards
+// 5. When the chooses two cards that do not match, flip them back over
+// 6. When the user wins or loses, indicate as much
+// 7. All screens should be generated via JavaScript templates
